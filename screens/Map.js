a624d7aa5dd953f1fbf1433f7b194e365b8091e3
Rostyslav <rostyslavpasternak@gmail.com>
2023-12-22 20:52:18 +0200

    add pins on  map

--- screens/Map.js
+++ screens/Map.js
@@ -1,13 +1,48 @@
-import React from "react";
-import { StyleSheet, View, Text, Dimensions } from "react-native";
-import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import React, { useState } from "react";
+import { StyleSheet, View, Text, Dimensions, Image, TouchableOpacity } from "react-native";
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 import { useNavigation } from "@react-navigation/native";
 
 const screenWidth = Dimensions.get("window").width;
 const screenHeight = Dimensions.get("window").height;
 
+const data = [
+  { 
+    id: 1, 
+    name: "Магазин Атб",
+    street: "Вулиця Шевченка, 2а",
+    distance: "500м",
+    imageSource: require('../assets/atb500.png'),
+    coordinate: {
+      latitude: 49.842606,
+      longitude: 24.025363,
+    }
+  },
+  { 
+    id: 2, 
+    name: "Магазин Атб",
+    street: "Вулиця Шевченка, 2а",
+    distance: "500м",
+    imageSource: require('../assets/atb500.png'), 
+    coordinate: {
+      latitude: 49.839426,
+      longitude: 24.022695,
+    }
+  },
+];
+
 const Map = () => {
-  const navigator = useNavigation();
+  const navigation = useNavigation();
+  const [userLocation, setUserLocation] = useState(null);
+  const [selectedPlace, setSelectedPlace] = useState(null);
+
+  const handleMarkerPress = (place) => {
+    setSelectedPlace(place);
+  };
+
+  const closeInfo = () => {
+    setSelectedPlace(null);
+  };
 
   return (
     <View style={styles.container}>
@@ -17,14 +52,37 @@ const Map = () => {
       <MapView
         provider={PROVIDER_GOOGLE}
         style={styles.map}
+        showsUserLocation={true}
         region={{
-          latitude: 49.8382600,
-          longitude: 24.0232400,
-          latitudeDelta: 0.09,
-          longitudeDelta: 0.009
-          ,
+          latitude: userLocation?.latitude || 0,
+          longitude: userLocation?.longitude || 0,
+          latitudeDelta: 0.9,
+          longitudeDelta: 0.9,
         }}
-      />
+      >
+        {data.map((place) => (
+          <Marker
+            key={place.id}
+            coordinate={place.coordinate}
+            onPress={() => handleMarkerPress(place)}
+          />
+        ))}
+      </MapView>
+
+      {selectedPlace && (
+        <View style={styles.shop}>
+          <Image source={selectedPlace.imageSource} style={styles.image} />
+          <View style={styles.textContainer}>
+            <Text style={styles.shopName}>{selectedPlace.name}</Text>
+            <Text style={styles.street}>{selectedPlace.street}</Text>
+          </View>
+          <View>
+            <TouchableOpacity style={styles.closeButton} onPress={closeInfo}>
+              <Image source={require('../assets/cross.png')} />
+            </TouchableOpacity>
+          </View>
+        </View>
+      )}
     </View>
   );
 };
@@ -54,6 +112,43 @@ const styles = StyleSheet.create({
     ...StyleSheet.absoluteFillObject,
     flex: 1,
     zIndex: -1,
+  },
+  shop: {
+    borderWidth: 2,
+    borderColor: "#B6B6B6",
+    borderRadius: 25,
+    width: screenWidth * 0.9,
+    height: screenHeight * 0.13,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingLeft: 10,
+    paddingRight: 10,
+    marginBottom: 20,
+    position: "absolute",
+    bottom: 10,
+    left: screenWidth * 0.05,
+    right: screenWidth * 0.05,
+    backgroundColor: "#fff",
+  },
+  image: {
+    width: screenWidth * 0.18,
+    height: screenWidth * 0.18,
+    borderRadius: 50,
+  },
+  textContainer: {
+    flex: 1,
+    justifyContent: "center",
+    marginLeft: 10,
+  },
+  shopName: {
+    fontWeight: "bold",
+  },
+  street: {
+    color: "#808080",
+  },
+  closeButton: {
+    backgroundColor: "transparent", 
   },
 });
 
