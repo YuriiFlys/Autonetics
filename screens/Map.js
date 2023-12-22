76fc5d5a40987cd929e4139136fbba12368e7a1b
Rostyslav <rostyslavpasternak@gmail.com>
2023-12-22 18:37:46 +0200

    create map

--- screens/Map.js
+++ screens/Map.js
@@ -1,7 +1,6 @@
-import * as React from "react";
-import { StyleSheet, View, Text, TouchableOpacity, Dimensions } from "react-native";
-import { Image } from "expo-image";
-import { Color, FontFamily, FontSize, Border } from "../GlobalStyles";
+import React from "react";
+import { StyleSheet, View, Text, Dimensions } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 import { useNavigation } from "@react-navigation/native";
 
 const screenWidth = Dimensions.get("window").width;
@@ -9,51 +8,53 @@ const screenHeight = Dimensions.get("window").height;
 
 const Map = () => {
   const navigator = useNavigation();
+
   return (
-    <View style={[styles.container]}>
-
-          <View style={styles.topRectangle}/>
-          <Text style={styles.labelText}>Карта </Text>
-
-        </View>
+    <View style={styles.container}>
+      <View style={styles.topRectangle}>
+        <Text style={styles.labelText}>Карта</Text>
+      </View>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        region={{
+          latitude: 49.8382600,
+          longitude: 24.0232400,
+          latitudeDelta: 0.09,
+          longitudeDelta: 0.009
+          ,
+        }}
+      />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems:'center',
-    backgroundColor: 'transparent',
-    position: 'relative',
+    flexDirection: "column",
+    backgroundColor: "transparent",
+    position: "relative",
   },
   labelText: {
-    position: 'absolute',
-    top: screenHeight*0.01,
-    fontFamily: 'PalanquinDark-Regular',
+    position: "absolute",
+    top: screenHeight * 0.01,
+    fontFamily: "PalanquinDark-Regular",
     fontSize: 18,
-    color: '#404040',
-  },
-  logoIcon: {
-    alignSelf: 'center',
-    height: screenWidth*0.13,
-    width: screenWidth*0.13,
-    position: 'absolute',
-    top: screenHeight*0,
-    left: screenWidth*0.04,
+    color: "#404040",
   },
   topRectangle: {
-    position: 'absolute',
-    height: screenHeight*0.15,
+    height: screenHeight * 0.15,
     width: screenWidth,
-    top: 0,
-    backgroundColor: '#fff',
-
-    alignItems: 'center',
-
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  
+  map: {
+    ...StyleSheet.absoluteFillObject,
+    flex: 1,
+    zIndex: -1,
+  },
 });
 
 export default Map;
