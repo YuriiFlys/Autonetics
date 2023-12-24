f6f238923f1bcf554eea1f172b93129967945192
LevPredko <levpredko@gmail.com>
2023-12-24 13:37:04 +0200

    added Basket

--- screens/Basket.js
+++ screens/Basket.js
@@ -3,110 +3,205 @@ import { StyleSheet, View, Text, TouchableOpacity, Dimensions } from "react-nati
 import { Image } from "expo-image";
 import { Color, FontFamily, FontSize, Border } from "../GlobalStyles";
 import { useNavigation } from "@react-navigation/native";
-import QRCode from 'react-native-qrcode-svg';
-import BottomSheet from '@gorhom/bottom-sheet';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import Logo from '../components/Logo';
+
 const screenWidth = Dimensions.get("window").width;
 const screenHeight = Dimensions.get("window").height;
 
 const Basket = () => {
   const navigator = useNavigation();
-  const bottomSheetRef = React.useRef();
-  const [snapPointIndex, setSnapPointIndex] = React.useState(0);
-
+  const bottomSheetRef = React.useRef(null);
+  const toggleFavorites = () => {
+    setFavoritesVisible(!favoritesVisible);
+  };
+  const toggleHystory = () => {
+    setHystoryVisible(!hystoryVisible);
+  };
+  
   return (
-//     <GestureHandlerRootView style={{ flex: 1 }}>
-//       <View style={[styles.container]}>
-//           <View style={styles.topRectangle}/>
-//           <Text style={styles.labelText}>Історія та улюблені </Text>
-//           <TouchableOpacity
-//       style={styles.logoIcon}
-//       onPress={() => navigator.navigate('Home', { screen: 'Головне меню' })}>
-//   <Image
-//     style={styles.logoIcon}
-//     contentFit="contain"
-//     source={require("../assets/logo1.png")}
-//   />
-// </TouchableOpacity>
-//         <BottomSheet
-//           ref={bottomSheetRef}
-//           index={0}
-//           snapPoints={['10%','75%']}
-//           onChange={index => setSnapPointIndex(index)}
-//         >
-//           <View style={styles.qrContainer}>
-//           <Text style={[styles.sheetText, snapPointIndex === 0 ? styles.closedBasketText : styles.BasketText]}>
-//               {snapPointIndex === 0 ? "До сплати" : "Ваша корзина"}
-//             </Text>
-//             <QRCode
-//               value="https://www.wikipedia.org/"
-//               size={screenHeight*0.4}
-//               color="black"
-//               backgroundColor="white"
-//             />
-//           </View>
-//         </BottomSheet>
-//         </View>
-//     </GestureHandlerRootView>
-    <View>      
-      <Logo name={"Історія та улюблені"} />
+    <View style={[styles.container]}>
+      <Text style={styles.mainText}>Історія та улюблені</Text>
+      <TouchableOpacity
+        style={styles.logoIcon}
+        onPress={() => navigator.navigate('Home', { screen: 'Головне меню' })}>
+        <Image
+          style={styles.logoIcon}
+          contentFit="contain"
+          source={require("../assets/logo1.png")}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.favoritesButton}
+        onPress={toggleFavorites}
+      >
+        <View style={styles.favoritesButton}>
+        <Text style={styles.favoritesButtonText}>Улюблені </Text>
+        <Text style={styles.favoritesButtonArrow}>&gt;</Text>
+        </View>
+        <View style={styles.horizontalLineFavorites}></View>
+        
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.historyButton}
+        onPress={() => toggleHystory}
+      >
+        <View style={styles.historyButton}>
+          <Text style={styles.historyButtonText}>Історія </Text>
+          <Text style={styles.historyButtonArrow}>&gt;</Text>
+        </View>
+        <View style={styles.horizontalLineHistory}></View>
+      </TouchableOpacity>
     </View>
+
   );
 };
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
     justifyContent: 'center',
-    alignItems:'center',
-    backgroundColor: 'transparent',
+    alignItems: 'center',
+    backgroundColor: '#FFF',
     position: 'relative',
   },
-  labelText: {
+  mainText: {
     position: 'absolute',
-    top: screenHeight*0.1,
+    top: screenHeight * 0.102,
+    left: screenWidth * 0.337,
     fontFamily: 'PalanquinDark-Regular',
     fontSize: 18,
     color: '#404040',
   },
-  BasketText:{
-    top:0,
-    
+  favoritesButton: {
+    position: "absolute",
+    top: screenHeight * 0.09,
+    left: 0,
+    right: 0,
+    height: 50,
+    flexDirection: 'row', 
+    justifyContent: "center",
+    alignItems: "baseline"
   },
-  closedBasketText:{
-    top:0,
-    left:screenWidth*0.1,
-  },
-  sheetText:{
-    top:0,
-    position:"absolute",
-    fontFamily: 'PalanquinDark-Regular',
+  favoritesButtonText: {
+    fontFamily: "PalanquinDark-Regular",
     fontSize: 18,
-    color: '#404040',
+    color: "#B6B6B6",
+    marginRight: 272, 
+    marginBottom: 20
   },
+  favoritesButtonArrow: {
+    fontFamily: "PalanquinDark-Regular",
+    fontSize: 30,
+    color: "#B6B6B6",
+  },
+  horizontalLineFavorites: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: screenHeight * 0.15, 
+    borderBottomWidth: 1,       
+    borderBottomColor: '#B6B6B6',  
+  },
+  historyButton: {
+    position: "absolute",
+    top: screenHeight * 0.118,
+    left: 0,
+    right: 0,
+    height: 50,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "baseline",
+  },
+  historyButtonText: {
+    fontFamily: "PalanquinDark-Regular",
+    fontSize: 18,
+    color: "#B6B6B6",
+    marginRight: 295,
+    marginBottom: 20,
+  },
+  historyButtonArrow: {
+    fontFamily: "PalanquinDark-Regular",
+    fontSize: 30,
+    color: "#B6B6B6",
+  },
+  horizontalLineHistory: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: screenHeight * 0.18,
+    borderBottomWidth: 1,
+    borderBottomColor: "#B6B6B6",
+  },
+
   logoIcon: {
     alignSelf: 'center',
-    height: screenWidth*0.1299,
-    width: screenWidth*0.1299,
+    height: screenWidth * 0.1299,
+    width: screenWidth * 0.1299,
     position: 'absolute',
-    top: screenHeight*0.0446,
-    left: screenWidth*0.04,
-  },
-  qrContainer: { 
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    top: screenHeight * 0.0446,
+    left: screenWidth * 0.04,
   },
   topRectangle: {
     position: 'absolute',
-    height: screenHeight*0.15,
+    height: screenHeight * 0.15,
     width: screenWidth,
     top: 0,
     backgroundColor: '#fff',
     alignItems: 'center',
 
   },
+  mainWidgetView: {
+    height: screenWidth * 0.4,
+    width: screenWidth * 0.9,
+    position: 'absolute',
+    top: screenHeight * 0.2,
+    left: screenWidth * 0.05,
+    backgroundColor: 'lightgrey',
+    borderRadius: 20,
+    overflow: 'hidden',
+  },
+  widgetInfoRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    position: 'absolute',
+    top: '5%',
+    left: 0,
+    right: 0,
+  },
+  widgetProductMainInfo: {
+    fontFamily: "PalanquinDark-Regular",
+    fontSize: 24,
+    color: "#404040",
+  },
+  widgetImageRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    top: '15%',
+    left: 0,
+    right: 0,
+    height: screenHeight * 0.25,
+    width: screenWidth * 0.75,
+  },
+  widgetProductImage: {
+    width: '70%',
+    height: '50%',
+    objectFit: 'contain',
+  },
+  
+  widgetProductBrandLogo: {
+    width: '30%',
+    height: '30%',
+    objectFit: 'contain',
+  },
+  widgetProductDescription: {
+    position: 'absolute',
+    top: '0%',
+    fontFamily: "PalanquinDark-Regular",
+    fontSize: 18,
+    color: "#404040",
+  }
+
 });
 export default Basket;
