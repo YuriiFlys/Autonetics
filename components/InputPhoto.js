8c384797abe0862d85bd38d0f089db7d9f5d4830
Rostyslav <rostyslavpasternak@gmail.com>
2024-04-27 00:55:59 +0300

    upload photo of new product

--- components/InputPhoto.js
+++ components/InputPhoto.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   StyleSheet,
   SafeAreaView,
@@ -6,28 +6,111 @@ import {
   Text,
   View,
   Button,
+  FlatList,
+  ScrollView,
+  TouchableOpacity,
 } from "react-native";
 import { Color, FontFamily, FontSize } from "../GlobalStyles";
+import * as ImagePicker from "expo-image-picker";
+import { Image } from "expo-image";
 const screenWidth = Dimensions.get("window").width;
 const screenHeight = Dimensions.get("window").height;
 
 const InputPhoto = () => {
+  const [image, setImage] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const libraryStatus =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
+      if (libraryStatus.status !== "granted") {
+        alert("Sorry, we need camera roll permissions to make this work!");
+      }
+
+      const cameraStatus = await ImagePicker.requestCameraPermissionsAsync();
+      if (cameraStatus.status !== "granted") {
+        alert("Sorry, we need camera permissions to make this work!");
+      }
+    })();
+  }, []);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      // allowsEditing: true,
+      aspect: [16, 9],
+      quality: 2,
+    });
+
+    if (!result.cancelled) {
+      console.log("result.uri", result);
+      setImage(image.concat(result));
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Input Photo</Text>
+      <ScrollView style={styles.containerImage} horizontal={true}>
+        {image.map((item) => {
+          return (
+            <View
+              style={{
+                flex: 1,
+                width: 100,
+                margin: 10,
+              }}
+            >
+              <Image source={{ uri: item.uri }} style={styles.image} />
+            </View>
+          );
+        })}
+        <TouchableOpacity
+          onPress={pickImage}
+          style={{
+            flex: 1,
+            width: 100,
+            margin: 20,
+            alignItems: "center",
+            justifyContent: "center",
+            borderColor: Color.colorLightGray,
+            borderWidth: 2,
+            borderRadius: 16,
+            marginRight: 20,
+          }}
+        >
+          <Text>+</Text>
+        </TouchableOpacity>
+      </ScrollView>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    width: "90%",
-    padding: 20,
+    width: screenWidth * 0.9,
   },
   text: {
     fontFamily: FontFamily.CommissioneMedium,
     fontSize: FontSize.size_xs,
     color: Color.colorLightGray,
+    margin: 10,
+  },
+  containerImage: {
+    height: "33%",
+    width: 0.9 * screenWidth,
+    borderColor: Color.colorLightGray,
+    borderWidth: 2,
+    borderRadius: 16,
+    flexDirection: "row",
+  },
+
+  image: {
+    height: "100%",
+    flex: 1,
+    contentFit: "contain",
+    margin: 10,
+    // backgroundColor: Color.colorLightGray,
   },
 });
 
--- screens/Admin/AddNewProduct.js
+++ screens/Admin/AddNewProduct.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { StyleSheet, SafeAreaView, Dimensions } from "react-native";
+import {
+  StyleSheet,
+  SafeAreaView,
+  Dimensions,
+  TouchableOpacity,
+  Text,
+  ScrollView,
+} from "react-native";
 import Logo from "../../components/Logo";
 import { Color, FontFamily, FontSize } from "../../GlobalStyles";
 import InputField from "../../components/InputField.js";
@@ -10,11 +17,16 @@ const screenHeight = Dimensions.get("window").height;
 const AddProductsScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <InputPhoto />
-      <InputField />
-      <InputField />
-      <InputField />
-      <InputField />
+      <ScrollView>
+        <InputPhoto />
+        <InputField />
+        <InputField />
+        <InputField />
+        <InputField />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttontext}>Додати товар</Text>
+        </TouchableOpacity>
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -24,6 +36,20 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     backgroundColor: Color.colorWhite,
+  },
+  button: {
+    width: "90%",
+    height: 50,
+    backgroundColor: Color.colorDarkBlue,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    marginVertical: 10,
+  },
+  buttontext: {
+    fontFamily: FontFamily.CommissioneMedium,
+    fontSize: FontSize.size_m,
+    color: Color.colorWhite,
   },
 });
 
