626658862ac3701de4c275a346029229154352d6
Rostyslav <rostyslavpasternak@gmail.com>
2024-05-17 13:46:00 +0300

    add photo

--- components/InputPhoto.js
+++ components/InputPhoto.js
@@ -5,30 +5,30 @@ import {
   Dimensions,
   Text,
   View,
-  Button,
-  FlatList,
   ScrollView,
   TouchableOpacity,
 } from "react-native";
 import { Color, FontFamily, FontSize } from "../GlobalStyles";
 import * as ImagePicker from "expo-image-picker";
 import { Image } from "expo-image";
+
 const screenWidth = Dimensions.get("window").width;
 const screenHeight = Dimensions.get("window").height;
 
 const InputPhoto = () => {
-  const [image, setImage] = useState([]);
+  const [images, setImages] = useState([]);
 
   useEffect(() => {
     (async () => {
-      const libraryStatus =
+      const { status: libraryStatus } =
         await ImagePicker.requestMediaLibraryPermissionsAsync();
-      if (libraryStatus.status !== "granted") {
+      if (libraryStatus !== "granted") {
         alert("Sorry, we need camera roll permissions to make this work!");
       }
 
-      const cameraStatus = await ImagePicker.requestCameraPermissionsAsync();
-      if (cameraStatus.status !== "granted") {
+      const { status: cameraStatus } =
+        await ImagePicker.requestCameraPermissionsAsync();
+      if (cameraStatus !== "granted") {
         alert("Sorry, we need camera permissions to make this work!");
       }
     })();
@@ -37,81 +37,81 @@ const InputPhoto = () => {
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      // allowsEditing: true,
-      aspect: [16, 9],
-      quality: 2,
+      quality: 1, // Quality should be between 0 and 1
     });
 
-    if (!result.cancelled) {
-      console.log("result.uri", result);
-      setImage(image.concat(result));
+    if (!result.canceled && result.assets && result.assets.length > 0) {
+      const newImages = result.assets.map((asset) => ({ uri: asset.uri }));
+      setImages([...images, ...newImages]);
     }
   };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Input Photo</Text>
-      <ScrollView style={styles.containerImage} horizontal={true}>
-        {image.map((item) => {
-          return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text style={styles.text}>Input Photo</Text>
+        <ScrollView style={styles.containerImage} horizontal={true}>
+          {images.map((item, index) => (
             <View
-              style={{
-                flex: 1,
-                width: 100,
-                margin: 10,
-              }}
+              key={index} // Adding key for each item
+              style={styles.imageContainer}
             >
               <Image source={{ uri: item.uri }} style={styles.image} />
             </View>
-          );
-        })}
-        <TouchableOpacity
-          onPress={pickImage}
-          style={{
-            flex: 1,
-            width: 100,
-            margin: 20,
-            alignItems: "center",
-            justifyContent: "center",
-            borderColor: Color.colorLightGray,
-            borderWidth: 2,
-            borderRadius: 16,
-            marginRight: 20,
-          }}
-        >
-          <Text>+</Text>
-        </TouchableOpacity>
-      </ScrollView>
-    </View>
+          ))}
+          <TouchableOpacity onPress={pickImage} style={styles.addButton}>
+            <Text style={styles.addButtonText}>+</Text>
+          </TouchableOpacity>
+        </ScrollView>
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
   container: {
     width: screenWidth * 0.9,
     height: screenHeight * 0.25,
+    marginHorizontal: screenWidth * 0.05,
   },
   text: {
     fontFamily: FontFamily.CommissioneMedium,
     fontSize: FontSize.size_xs,
     color: Color.colorLightGray,
-    margin: 10,
+    marginBottom: 10,
   },
   containerImage: {
-    height: "33%",
-    width: 0.9 * screenWidth,
+    height: "80%",
     borderColor: Color.colorDarkBlue,
     borderWidth: 1,
     borderRadius: 16,
     flexDirection: "row",
   },
-
-  image: {
-    height: "100%",
-    flex: 1,
-    contentFit: "contain",
+  imageContainer: {
+    width: 100,
     margin: 10,
-    // backgroundColor: Color.colorLightGray,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 16,
+  },
+  addButton: {
+    width: 100,
+    margin: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    borderColor: Color.colorLightGray,
+    borderWidth: 2,
+    borderRadius: 16,
+  },
+  addButtonText: {
+    fontSize: 24,
+    color: Color.colorLightGray,
   },
 });
 
